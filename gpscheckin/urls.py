28b2a7952aa2e239e0a17ca953d0729b78b57d70
pongvarid <pongvarid@gmail.com>
2020-07-26 17:43:31 +0700

    add frontend

--- gpscheckin/urls.py
+++ gpscheckin/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include 
 from checkin.serializer.RegisterSerializer import UserRegistrationView as reg
+from checkin.views.frontend import home as fviews 
 from checkin.views.auth365 import Callback
 from django.conf.urls.static import static 
 urlpatterns = [ 
@@ -25,7 +26,7 @@ urlpatterns = [
     path('api/auth/register-full/', reg.as_view(), name="rest_name_register"),
     path('auth/live',Callback.as_view()),
     path('api/', include("checkin.urls")),  
-    
+    path('checkin/', fviews),
     # path('', include("checkin.api.urls")),
    # path('', include("proflie.api.urls"))
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
